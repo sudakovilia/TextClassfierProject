699d915edc817a8bf5432fcdf643a45a601cf3fb
ilysko <ilya.lysko@gmail.com>
2017-08-15 13:03:32 +0300

    Created CountVectorizer (without parallel mode, minDf and maxDf parameteres).

--- src/ru/caf82/result/machinelearning/preprocessing/TfidfVectorizer.java
+++ src/ru/caf82/result/machinelearning/preprocessing/TfidfVectorizer.java
@@ -1,7 +1,31 @@
 package ru.caf82.result.machinelearning.preprocessing;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by ilysko on 03.08.17.
  */
 public class TfidfVectorizer implements SequenceProcessor, Transformer {
+    @Override
+    public List<Map<String, Integer>> fitAndTransform(List<String> listOfTexts) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void fit(List<String> listOfTexts) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public List<String> preprocess(String text) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public List<Map<String, Integer>> transform(List<String> listOfTexts) {
+        throw new NotImplementedException();
+    }
 }
--- src/ru/caf82/result/machinelearning/preprocessing/Transformer.java
+++ src/ru/caf82/result/machinelearning/preprocessing/Transformer.java
@@ -4,4 +4,5 @@ package ru.caf82.result.machinelearning.preprocessing;
  * Created by ilysko on 03.08.17.
  */
 public interface Transformer {
+//    TODO do we really need it??
 }
