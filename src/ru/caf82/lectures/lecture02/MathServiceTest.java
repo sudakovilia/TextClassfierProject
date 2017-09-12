f0bb639f1811c43d08e1128e0cff1e0fd221d5e8
sudakovilia <sudakovilia@gmail.com>
2017-09-12 15:06:52 +0300

    some corrections

--- src/ru/caf82/lectures/lecture02/MathServiceTest.java
+++ src/ru/caf82/lectures/lecture02/MathServiceTest.java
@@ -2,24 +2,26 @@ package ru.caf82.lectures.lecture02;
 
 public class MathServiceTest {
     public static void main(String[] args) {
-        double[] f = {2, 2};
-        double[] s = {5, 6};
-        System.out.println(MathService.dotProduct(f, s));
+        double[] f = {2.05, 2.78};
+        double[] s = {5.146, .0871};
+        System.out.println("Dot product = " + String.format("%.3f", MathService.dotProduct(f, s)));
+        System.out.println("Sigmoid = " + MathService.sigmoid(.05));
     }
 }
 
-class MathService{
-    public static double dotProduct(double[] a, double[] b){
-        double result = 0;
-        if (a.length == b.length){
-            for (int i=0; i<a.length; i++){
-                result += a[i]*b[i];
+class MathService {
+    public static double dotProduct(double[] a, double[] b) {
+        if (a.length == b.length) {
+            double result = 0;
+            for (int i = 0; i < a.length; i++) {
+                result += a[i] * b[i];
             }
-        } else return 0;
-
-        return result;
-    }
-     public static double sigmoid(double x){
+            return result;
+        }
         return 0;
-     }
+    }
+
+    public static double sigmoid(double x) {
+        return 1 / (1 + Math.exp(-x));
+    }
 }
