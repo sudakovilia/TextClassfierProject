633da34a4fef53509158f2532260262829738cf1
sudakovilia <sudakovilia@gmail.com>
2017-09-12 13:42:49 +0300

    seems like MathService works fine

--- src/ru/caf82/lectures/lecture02/MathService.java
+++
@@ -1,7 +0,0 @@
-package ru.caf82.lectures.lecture02;
-
-public class MathService {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ src/ru/caf82/lectures/lecture02/MathServiceTest.java
@@ -0,0 +1,22 @@
+package ru.caf82.lectures.lecture02;
+
+public class MathServiceTest {
+    public static void main(String[] args) {
+        double[] f = {2, 2};
+        double[] s = {5, 6};
+        System.out.println(MathService.dotProduct(f, s));
+    }
+}
+
+class MathService{
+    public static double dotProduct(double[] a, double[] b){
+        double result = 0;
+        if (a.length == b.length){
+            for (int i=0; i<a.length; i++){
+                result += a[i]*b[i];
+            }
+        } else return 0;
+
+        return result;
+    }
+}
